e44315c2837b6e97daa9cbd0e83aaee25d4488dd
Neal Williams <neal@lexome.com>
2024-04-07 19:08:11 -0400

    fix types, move to npm

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -1,190 +1,8 @@
-import React from 'react';
+import React, { CSSProperties } from 'react';
 // import { createStyled, BaseStyleProps, StyleEngine } from '../../core/src';
 import { createStyled, StyleEngine, StyleKitNProvider } from '@style-kit-n/core';
 
 import { htmlAttributes } from './elementAttributes';
-
-type StyleProps = {
-  bg?: string,
-  color?: string,
-  borderColor?: string,
-  w?: string,
-  h?: string,
-  m?: string,
-  mt?: string,
-  mb?: string,
-  ml?: string,
-  mr?: string,
-  my?: string,
-  mx?: string,
-  p?: string,
-  pt?: string,
-  pb?: string,
-  pl?: string,
-  pr?: string,
-  py?: string,
-  px?: string,
-  typography?: string,
-  fontSize?: string,
-  lineHeight?: string,
-  fontFamily?: string,
-  fontWeight?: string
-}
-
-const propertyStyleMap = {
-  bg: ({
-    props,
-    theme
-  }) => ({
-    backgroundColor: theme.colors[props.bg] || props.bg
-  }),
-  color: ({
-    props,
-    theme
-  }) => ({
-    color: theme.colors[props.color] || props.color
-  }),
-  borderColor: ({
-    props,
-    theme
-  }) => ({
-    borderColor: theme.colors[props.borderColor] || props.borderColor
-  }),
-  w: ({
-    props
-  }) => ({
-    width: props.w
-  }),
-  h: ({
-    props
-  }) => ({
-    height: props.h
-  }),
-  m: ({
-    props,
-    theme,
-  }) => ({
-    margin: theme.space[props.m] || props.m
-  }),
-  mt: ({
-    props,
-    theme,
-  }) => ({
-    marginTop: theme.space[props.mt] || props.mt
-  }),
-  mb: ({
-    props,
-    theme,
-  }) => ({
-    marginBottom: theme.space[props.mb] || props.mb
-  }),
-  ml: ({
-    props,
-    theme,
-  }) => ({
-    marginLeft: theme.space[props.ml] || props.ml
-  }),
-  mr: ({
-    props,
-    theme,
-  }) => ({
-    marginRight: theme.space[props.mr] || props.mr
-  }),
-  my: ({
-    props,
-    theme,
-  }) => ({
-    marginTop: theme.space[props.my] || props.my,
-    marginBottom: theme.space[props.my] || props.my
-  }),
-  mx: ({
-    props,
-    theme,
-  }) => ({
-    marginLeft: theme.space[props.mx] || props.mx,
-    marginRight: theme.space[props.mx] || props.mx
-  }),
-  p: ({
-    props,
-    theme,
-  }) => ({
-    padding: theme.space[props.p] || props.p
-  }),
-  pt: ({
-    props,
-    theme,
-  }) => ({
-    paddingTop: theme.space[props.pt] || props.pt
-  }),
-  pb: ({
-    props,
-    theme,
-  }) => ({
-    paddingBottom: theme.space[props.pb] || props.pb
-  }),
-  pl: ({
-    props,
-    theme,
-  }) => ({
-    paddingLeft: theme.space[props.pl] || props.pl
-  }),
-  pr: ({
-    props,
-    theme,
-  }) => ({
-    paddingRight: theme.space[props.pr] || props.pr
-  }),
-  py: ({
-    props,
-    theme,
-  }) => ({
-    paddingTop: theme.space[props.py] || props.py,
-    paddingBottom: theme.space[props.py] || props.py
-  }),
-  px: ({
-    props,
-    theme,
-  }) => ({
-    paddingLeft: theme.space[props.px] || props.px,
-    paddingRight: theme.space[props.px] || props.px
-  }),
-  typography: ({
-    props,
-    theme
-  }) => {
-    const typography = theme.typography[props.typography]
-    return {
-      fontFamily: theme.fontFamily[typography.fontFamily],
-      fontSize: theme.fontSize[typography.fontSize],
-      fontWeight: theme.fontWeight[typography.fontWeight] as any,
-      lineHeight: theme.lineHeight[typography.lineHeight]
-    }
-  },
-  fontSize: ({
-    props,
-    theme
-  }) => ({
-    fontSize: theme.fontSize[props.fontSize]
-  }),
-  lineHeight: ({
-    props,
-    theme
-  }) => ({
-    lineHeight: theme.lineHeight[props.lineHeight]
-  }),
-  fontFamily: ({
-    props,
-    theme
-  }) => ({
-    fontFamily: theme.fontFamily[props.fontFamily]
-  }),
-  fontWeight: ({
-    props,
-    theme
-  }) => ({
-    fontWeight: theme.fontWeight[props.fontWeight] as any
-  }),
-}
 
 type WebTheme = {
   colors: {
@@ -213,6 +31,203 @@ type WebTheme = {
       lineHeight: string
     }
   }
+}
+
+type StyleProps = {
+  bg?: string,
+  color?: string,
+  borderColor?: string,
+  w?: string,
+  h?: string,
+  m?: string,
+  mt?: string,
+  mb?: string,
+  ml?: string,
+  mr?: string,
+  my?: string,
+  mx?: string,
+  p?: string,
+  pt?: string,
+  pb?: string,
+  pl?: string,
+  pr?: string,
+  py?: string,
+  px?: string,
+  typography?: string,
+  fontSize?: string,
+  lineHeight?: string,
+  fontFamily?: string,
+  fontWeight?: string,
+  display?: CSSProperties['display'],
+  flexDirection?: CSSProperties['flexDirection']
+}
+
+type StyleHelper = (params: {
+  props: StyleProps,
+  theme: WebTheme
+}) => React.CSSProperties
+
+const propertyStyleMap: {
+  [key in keyof StyleProps]: StyleHelper
+} = {
+  display: ({props}) => ({
+    display: props.display
+  }),
+  flexDirection: ({props}) => ({
+    flexDirection: props.flexDirection
+  }),
+  bg: ({
+    props,
+    theme
+  }) => ({
+    backgroundColor: theme.colors[props.bg!] || props.bg
+  }),
+  color: ({
+    props,
+    theme
+  }) => ({
+    color: theme.colors[props.color!] || props.color
+  }),
+  borderColor: ({
+    props,
+    theme
+  }) => ({
+    borderColor: theme.colors[props.borderColor!] || props.borderColor
+  }),
+  w: ({
+    props
+  }) => ({
+    width: props.w
+  }),
+  h: ({
+    props
+  }) => ({
+    height: props.h
+  }),
+  m: ({
+    props,
+    theme,
+  }) => ({
+    margin: theme.space[props.m!] || props.m
+  }),
+  mt: ({
+    props,
+    theme,
+  }) => ({
+    marginTop: theme.space[props.mt!] || props.mt
+  }),
+  mb: ({
+    props,
+    theme,
+  }) => ({
+    marginBottom: theme.space[props.mb!] || props.mb
+  }),
+  ml: ({
+    props,
+    theme,
+  }) => ({
+    marginLeft: theme.space[props.ml!] || props.ml
+  }),
+  mr: ({
+    props,
+    theme,
+  }) => ({
+    marginRight: theme.space[props.mr!] || props.mr
+  }),
+  my: ({
+    props,
+    theme,
+  }) => ({
+    marginTop: theme.space[props.my!] || props.my,
+    marginBottom: theme.space[props.my!] || props.my
+  }),
+  mx: ({
+    props,
+    theme,
+  }) => ({
+    marginLeft: theme.space[props.mx!] || props.mx,
+    marginRight: theme.space[props.mx!] || props.mx
+  }),
+  p: ({
+    props,
+    theme,
+  }) => ({
+    padding: theme.space[props.p!] || props.p
+  }),
+  pt: ({
+    props,
+    theme,
+  }) => ({
+    paddingTop: theme.space[props.pt!] || props.pt
+  }),
+  pb: ({
+    props,
+    theme,
+  }) => ({
+    paddingBottom: theme.space[props.pb!] || props.pb
+  }),
+  pl: ({
+    props,
+    theme,
+  }) => ({
+    paddingLeft: theme.space[props.pl!] || props.pl
+  }),
+  pr: ({
+    props,
+    theme,
+  }) => ({
+    paddingRight: theme.space[props.pr!] || props.pr
+  }),
+  py: ({
+    props,
+    theme,
+  }) => ({
+    paddingTop: theme.space[props.py!] || props.py,
+    paddingBottom: theme.space[props.py!] || props.py
+  }),
+  px: ({
+    props,
+    theme,
+  }) => ({
+    paddingLeft: theme.space[props.px!] || props.px,
+    paddingRight: theme.space[props.px!] || props.px
+  }),
+  typography: ({
+    props,
+    theme
+  }) => {
+    const typography = theme.typography[props.typography!]
+    return {
+      fontFamily: theme.fontFamily[typography.fontFamily],
+      fontSize: theme.fontSize[typography.fontSize],
+      fontWeight: theme.fontWeight[typography.fontWeight] as any,
+      lineHeight: theme.lineHeight[typography.lineHeight]
+    }
+  },
+  fontSize: ({
+    props,
+    theme
+  }) => ({
+    fontSize: theme.fontSize[props.fontSize!]
+  }),
+  lineHeight: ({
+    props,
+    theme
+  }) => ({
+    lineHeight: theme.lineHeight[props.lineHeight!]
+  }),
+  fontFamily: ({
+    props,
+    theme
+  }) => ({
+    fontFamily: theme.fontFamily[props.fontFamily!]
+  }),
+  fontWeight: ({
+    props,
+    theme
+  }) => ({
+    fontWeight: theme.fontWeight[props.fontWeight!] as any
+  }),
 }
 
 const webEngine: StyleEngine<
@@ -253,7 +268,13 @@ const webEngine: StyleEngine<
   getBaseProps: () => htmlAttributes
 };
 
-export const styled = createStyled(webEngine)
+export const styled = createStyled<
+  keyof JSX.IntrinsicElements,
+  StyleProps,
+  typeof webEngine,
+  string,
+  React.CSSProperties
+>(webEngine)
 
 export const Row = styled('div', {
   display: 'flex',
